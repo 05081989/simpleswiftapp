e1c0ba4281ba5b4539329907a01d029ec18bd20e
Lucky <sowmya.muni@gmail.com>
2021-07-22 08:12:04 -0400

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Lucky on 22/07/21.
+//  Copyright © 2021 Lucky. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
